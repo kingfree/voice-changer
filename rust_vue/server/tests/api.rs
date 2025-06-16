ac7b2435fe657dbb8a76f5f9ddc10e8fa6c2fb53
Kingfree <anytjf@gmail.com>
2025-06-16 08:21:17 +0800

    Add WebSocket test and tokio-tungstenite dev dependency

--- rust_vue/server/tests/api.rs
+++ rust_vue/server/tests/api.rs
@@ -2,9 +2,10 @@ use reqwest::Client;
 use reqwest::StatusCode;
 use server::app;
 use tokio::net::TcpListener;
+use tokio_tungstenite::connect_async;
 
 #[tokio::test]
-async fn test_version_and_openapi() {
+async fn test_endpoints_from_openapi() {
     let listener = TcpListener::bind("127.0.0.1:0").await.unwrap();
     let addr = listener.local_addr().unwrap();
     let app = app();
@@ -14,24 +15,31 @@ async fn test_version_and_openapi() {
 
     let client = Client::builder().build().unwrap();
 
-    let version = client
-        .get(format!("http://{}/version", addr))
-        .send()
-        .await
-        .unwrap()
-        .text()
-        .await
-        .unwrap();
-    assert_eq!(version, "0.1");
-
     let resp = client
         .get(format!("http://{}/openapi.json", addr))
         .send()
         .await
         .unwrap();
     assert_eq!(resp.status(), StatusCode::OK);
-    let json: serde_json::Value = resp.json().await.unwrap();
-    assert!(json["paths"].get("/version").is_some());
+    let spec: serde_json::Value = resp.json().await.unwrap();
+
+    if spec["paths"].get("/version").is_some() {
+        let version = client
+            .get(format!("http://{}/version", addr))
+            .send()
+            .await
+            .unwrap()
+            .text()
+            .await
+            .unwrap();
+        assert_eq!(version, "0.1");
+    }
+
+    if spec["paths"].get("/ws/audio").is_some() {
+        let url = format!("ws://{}/ws/audio", addr);
+        let (mut ws, _) = connect_async(&url).await.unwrap();
+        ws.close(None).await.unwrap();
+    }
 
     server.abort();
 }
